32910f8219e9444c9de9e2f692e59ab85bbc395f
Maxime Lefebvre <mabouka@MacBook-Pro-de-Maxime.local>
2024-03-18 12:38:06 -0300

    added lazy

--- wp-content/themes/mojo-v2/partials/caseAccordion.php
+++ wp-content/themes/mojo-v2/partials/caseAccordion.php
@@ -58,7 +58,7 @@
 
         <div class="caseAccordion__image">
         <?php if ($video): ?>
-            <video preload="auto" playsinline muted <?= $imageCard ? 'poster="' .$imageCard['sizes']['imagesslider'] .'"' : ''?>>
+            <video class="lazy" preload="auto" playsinline muted <?= $imageCard ? 'poster="' .$imageCard['sizes']['imagesslider'] .'"' : ''?>>
                 <source data-src="<?= $video ?>"  media="screen and (min-width: 601px)" type="video/mp4">
                 <source data-src="<?= $videoWebm ?>"  media="screen and (min-width: 601px)" type="video/webm">
             </video>
