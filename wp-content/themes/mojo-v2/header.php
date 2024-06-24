e24a5e837cf0728b28d1d14851fc0f638fb8aa08
Maxime Lefebvre <mabouka@MacBook-Pro-de-Maxime.local>
2024-06-24 12:34:18 +0200

    change analytics

--- wp-content/themes/mojo-v2/header.php
+++ wp-content/themes/mojo-v2/header.php
@@ -33,6 +33,9 @@
 
     <?php 
         $gtag = 'G-RS946CZMGV';
+
+        $gtag = 'GTM-WBZ8MLM';
+        $cookieConsent = false;
     ?>
     <!-- InView & JS -->
     <script>
@@ -54,6 +57,8 @@
 
     <!-- Google Tag Manager -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $gtag ?>"></script>
+    <?php if ($cookieConsent): ?>
+    
     <script>
         let data = {
             'ad_storage': 'denied',
@@ -106,16 +111,47 @@
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
 
+    <?php else: ?>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WBZ8MLM');</script>
+    <!-- End Google Tag Manager -->
+
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:4941036,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
+    <?php endif ?>
+
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.css">
 
 </head>
 
 <body <?php body_class(); ?> data-barba="wrapper">
 
+    <?php if ($cookieConsent): ?>
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $gtag ?>"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+    <?php else: ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WBZ8MLM"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php endif; ?>
 
     <div class="curtain">&nbsp;</div>
 
