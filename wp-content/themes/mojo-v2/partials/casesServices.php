5eab7b21341e1f838bc33414e4d757eeb58ebdd4
Maxime Lefebvre <mabouka@gmail.com>
2026-06-25 11:29:15 +0200

    fix card width

--- wp-content/themes/mojo-v2/partials/casesServices.php
+++ wp-content/themes/mojo-v2/partials/casesServices.php
@@ -11,30 +11,28 @@ $the_query = new WP_Query([
         <div class="casesServices__inside">
             <h2 class="casesServices__title"><?= $title; ?></h2>
             <?php if ($text): ?>
-            <div class="casesServices__text">
-                <?= $text; ?>
-            </div>
+                <div class="casesServices__text">
+                    <?= $text; ?>
+                </div>
             <?php endif ?>
 
             <?php if ($the_query->have_posts()) : ?>
-            <div class="casesServices__gridContainer">
-                <div class="casesServices__grid">
-                    <?php foreach ($the_query->posts as $key => $service) : ?>
+                <div class="casesServices__gridContainer">
+                    <div class="casesServices__grid">
+                        <?php foreach ($the_query->posts as $key => $service) : ?>
                             <?php getPartial('serviceCard', [
                                 'service' => $service,
-                                'modifier' => 'full'
+                                'modifier' => 'animated'
                             ]); ?>
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
-            </div>
-
             <?php endif; ?>
 
-
             <?php if ($button): ?>
-            <a class="casesServices__button btn btn--dark" href="<?= $button['url'] ?>">
-                <?= $button['title'] ?>
-            </a>
+                <a class="casesServices__button btn btn--dark" href="<?= $button['url'] ?>">
+                    <?= $button['title'] ?>
+                </a>
             <?php endif ?>
 
         </div>
